803408d34f273ddf01efdd1bb2092d3c6378b6c2
TRƯƠNG ĐỨC DUY <Duy.TruongDuc@vn.bosch.com>
2015-05-19 10:43:54 +0700

    chapter 04

---
+++ Ch04/Fragments/src/dauruy/fragments/SkeletonFragment.java
@@ -0,0 +1,18 @@
+package dauruy.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SkeletonFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		return super.onCreateView(inflater, container, savedInstanceState);
+		//return inflater.inflate(resource, root, attachToRoot);
+	}
+}
